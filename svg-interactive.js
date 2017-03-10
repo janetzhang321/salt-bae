1120a9668522bda3965a65c91059840aa9bd485d
nckng <nng3@stuy.edu>
2017-03-09 21:09:27 -0500

    done

--- svg-interactive.js
+++ svg-interactive.js
@@ -9,9 +9,9 @@ var rid;
 
 //clear
 var clearSVG = function(){
-	while (svg.lastChild) {
-		svg.removeChild(svg.lastChild);
-	};
+    while (svg.lastChild) {
+	svg.removeChild(svg.lastChild);
+    };
 
 };
 cb.addEventListener("click",clearSVG);
@@ -19,104 +19,103 @@ cb.addEventListener("click",clearSVG);
 
 //teleport
 var teleport = function(e) {
-	if (this.getAttribute("fill")=="green"){
-		this.remove();
-		var a = createCircle(e);
-		a.setAttribute("cx", Math.random()*width);	
-		a.setAttribute("cy", Math.random()*height);	
-		svg.appendChild(a);
-		e.stopPropagation();
-	};
+    if (this.getAttribute("fill")=="green"){
+	this.remove();
+	var a = createCircle(e);
+	a.setAttribute("cx", Math.random()*width);	
+	a.setAttribute("cy", Math.random()*height);
+	a.setAttribute("dx",1);
+	a.setAttribute("dy",1);
+	svg.appendChild(a);
+	e.stopPropagation();
+    };
 };
 
 
 //circleClick
 var circleClick = function(e) {
-	console.log("CIRCLE");
-	if (this.getAttribute("fill")!="green"){	
-		this.setAttribute("fill","green");
-		e.stopPropagation();
-	};
+    console.log("CIRCLE");
+    if (this.getAttribute("fill")!="green"){	
+	this.setAttribute("fill","green");
+	e.stopPropagation();
+    };
 };
 
 //createcircle
 var createCircle = function(e) {
-	var newDot = document.createElementNS( "http://www.w3.org/2000/svg", "circle");
-	newDot.setAttribute("cx",e.offsetX);
-	newDot.setAttribute("cy",e.offsetY);
-	newDot.setAttribute("r",20);
-	newDot.setAttribute("fill","goldenrod");
-	newDot.addEventListener("click", teleport);
-	newDot.addEventListener("click", circleClick);
-	return newDot;
-	
+    var newDot = document.createElementNS( "http://www.w3.org/2000/svg", "circle");
+    newDot.setAttribute("cx",e.offsetX);
+    newDot.setAttribute("cy",e.offsetY);
+    newDot.setAttribute("r",20);
+    newDot.setAttribute("fill","goldenrod");
+    newDot.setAttribute("dx",1);
+    newDot.setAttribute("dy",1);
+    newDot.addEventListener("click", teleport);
+    newDot.addEventListener("click", circleClick);
+    return newDot;
+    
 
 };
 
 
-var createCircleG = function(x,y,fill) {
-	var newDot = document.createElementNS( "http://www.w3.org/2000/svg", "circle");
-	newDot.setAttribute("cx",x);
-	newDot.setAttribute("cy",y);
-	newDot.setAttribute("r",20);
-	newDot.setAttribute("fill",fill);
-	newDot.addEventListener("click", teleport);
-	newDot.addEventListener("click", circleClick);
-	return newDot;
-	
+var createCircleG = function(x,y,fill,dx,dy) {
+    var newDot = document.createElementNS( "http://www.w3.org/2000/svg", "circle");
+    newDot.setAttribute("cx",x);
+    newDot.setAttribute("cy",y);
+    newDot.setAttribute("r",20);
+    newDot.setAttribute("fill",fill);
+    newDot.setAttribute("dx",dx);
+    newDot.setAttribute("dy",dy);
+    newDot.addEventListener("click", teleport);
+    newDot.addEventListener("click", circleClick);
+    return newDot;
+    
 
 };
 
 //addcircle
 var addCircle = function(e) {
-	console.log("add");
-	svg.appendChild(createCircle(e));
+    console.log("add");
+    svg.appendChild(createCircle(e));
 
 };
 
 
 var change = function(e) {
-	console.log("CIRCLE");
+    console.log("CIRCLE");
 };
 
 svg.addEventListener("click", addCircle);
 
 var moveCircles = function (e) {
 
-	window.cancelAnimationFrame(rid);
-	var x = 0;
-	var y = 0;
-	var dx = 1;
-	var dy = 1;
-	
-	var move = function(){
-		//console.log(c);
-		while (c < svg.children.length) {
-			console.log(c);
-			if (svg.children[c].getAttribute("cx") >= width || svg.children[x].getAttribute("cx") <= 0) {
-				dx=-1*dx;
-			}
-			else if (svg.children[c].getAttribute("cy") >= width || svg.children[x].getAttribute("cy") <= 0) {
-				dy=-1*dy;
-			}
-			else {
-				oldc = svg.children[c];
-				oldx = oldc.getAttribute("cx");
-				oldy = oldc.getAttribute("cy");
-				color = oldc.getAttribute("fill");
-				oldc.remove();
-				newc = createCircleG(oldx, oldy, color);
-				svg.appendChild(newc);
-			};
-			c++;
-			
-		};
-	
-	rid = window.requestAnimationFrame(move);
-	
+    window.cancelAnimationFrame(rid);
+    
+    var move = function(){
+
+	var balls = document.getElementsByTagName('circle');
+	for (var i =0; i < balls.length; i++){
+	    var ball = balls[i];
+	    var x = parseInt(ball.getAttribute("cx"));
+	    var y = parseInt(ball.getAttribute("cy"));
+	    var dx = parseInt(ball.getAttribute("dx"));
+	    var dy = parseInt(ball.getAttribute("dy"));
+	    if (x < 0 || x > width){
+		dx = -1 * dx;
+	    };
+	    if (y < 0 || y > width){
+		dy = -1 * dy;
+	    }
+	    ball.setAttribute("cx",x+dx);
+	    ball.setAttribute("cy",y+dy);
+	    ball.setAttribute("dx",dx);
+	    ball.setAttribute("dy",dy);
 	};
-		move();
+	rid = window.requestAnimationFrame(move);
+    };
+    move();
 };
+	    
+		
 
 moveb.addEventListener("click", moveCircles);
-
